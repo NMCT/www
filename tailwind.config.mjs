f03947321b55af6e1f118f93f2d9df5c41a492ef
MLoth <MLoth@users.noreply.github.com>
2024-02-13 17:42:54 +0100

    feat: some basic coponents

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3DC7F5',
+      },
+    },
   },
 
   plugins: [],
