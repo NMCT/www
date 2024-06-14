7bb7a1c7c5599824aad89313831021e5e442c58f
Marty <MLoth@users.noreply.github.com>
2024-06-14 12:09:25 +0200

    vendor: started CSS variable setup

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,23 @@
-// uno.config.ts
 import { defineConfig } from 'unocss'
 
+// root.css file is imported to get the root variables (imported in `/pages/index.astro`)
+
 export default defineConfig({
-  // extendTheme: {
-  // color: {},
-  // },
+  theme: {
+    colors: {
+      // tokens
+      default: 'var(--global-default)',
+      meta: 'var(--global-meta)',
+      brand: 'var(--global-brand)',
+      light: 'var(--global-light)',
+      'on-brand': 'var(--global-on-brand)',
+
+      /* Background */
+      page: 'var(--global-page)',
+      surface: 'var(--global-surface)',
+
+      /* Border */
+      border: 'var(--global-border)',
+    },
+  },
 })
