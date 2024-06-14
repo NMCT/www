cf17eae62adadb407b7098aee1a6f060ccc51e54
Marty <MLoth@users.noreply.github.com>
2024-06-14 12:59:42 +0200

    chore: minor clean up of variables & alpha

--- uno.config.ts
+++ uno.config.ts
@@ -5,15 +5,29 @@ import { defineConfig } from 'unocss'
 export default defineConfig({
   theme: {
     colors: {
+      neutral: {
+        50: '#f5f5f5',
+        100: '#e5e6e6',
+        200: '#cdcfd0',
+        300: '#b9bcbd',
+        400: '#a6aaab',
+        500: '#929798',
+        600: '#767c7d',
+        700: '#5c6061',
+        800: '#404446',
+        900: '#25292a',
+        950: '#0b0d0e',
+      },
+
       // tokens
-      default: 'var(--global-default)',
-      meta: 'var(--global-meta)',
-      brand: 'var(--global-brand)',
+      default: 'var(--global-default, <alpha-value>)',
+      meta: 'var(--global-meta, <alpha-value>)',
+      brand: 'var(--global-brand, <alpha-value>)',
       light: 'var(--global-light)',
       'on-brand': 'var(--global-on-brand)',
 
       /* Background */
-      page: 'var(--global-page)',
+      page: 'var(--global-page',
       surface: 'var(--global-surface)',
 
       /* Border */
